59d6b48cf61ed2abaf25962a0a8eb4a304bdec0f
Mauro <mauro_biazutti@hotmail.com>
2026-08-17 19:24:09 -0300

    refactor(apllication, index, controller): deixando o codigo com jeito ruby

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,17 @@
 class AboutController < ApplicationController
   def index
+    @technologies = [
+      "Ruby",
+      "Ruby on Rails",
+      "Linux",
+      "PostgreSQL",
+      "Docker",
+      "Git",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Tailwind CSS",
+      "Bootstrap"
+    ]
   end
 end
